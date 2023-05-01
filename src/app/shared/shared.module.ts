a22f7dd8eb807fbf6978a8bb8338303ab08c0d4c
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-01 15:09:25 +0400

    Added getFirstError pipe, used any type

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { NotFoundComponent } from './components/not-found/not-found.component';
-
 import { MaterialModule } from '../material.module';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { GetFirstErrorPipe } from './pipes';
 
 const COMPONENTS = [NotFoundComponent, LogoComponent];
 
 @NgModule({
-  declarations: [NotFoundComponent, LogoComponent],
+  declarations: [NotFoundComponent, LogoComponent, GetFirstErrorPipe],
   imports: [
     CommonModule,
     RouterModule,
