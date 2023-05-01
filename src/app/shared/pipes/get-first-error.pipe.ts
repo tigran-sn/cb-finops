befc775393aeaa9799c6a6fff6966652c0b5b277
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-01 15:19:13 +0400

    Fixed getFirstError pipe (with any return type)

--- src/app/shared/pipes/get-first-error.pipe.ts
+++ src/app/shared/pipes/get-first-error.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'getFirstError',
 })
 export class GetFirstErrorPipe implements PipeTransform {
-  transform(value: any, ...args: any[]): string | void {
+  transform(value: any, ...args: any[]): any {
     if (args.length > 1) {
       const controlName = args[0];
       for (const validationMessageKey in args[1][controlName]) {
