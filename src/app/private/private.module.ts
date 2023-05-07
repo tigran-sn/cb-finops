5b649183bcd566ea4f13aa228cc6c156260495fb
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-07 20:37:33 +0400

    Removed breadcrumbs component

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { PrivateComponent } from './private.component';
 import { SharedModule } from '../shared/shared.module';
 import { PrivateRoutingModule } from './private-routing.module';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { EditDialogComponent } from './components/edit-dialog/edit-dialog.component';
@@ -12,7 +11,6 @@ import { PermissionsGuard } from './services/guard';
 @NgModule({
   declarations: [
     PrivateComponent,
-    BreadcrumbsComponent,
     NavigationComponent,
     SummaryComponent,
     EditDialogComponent,
