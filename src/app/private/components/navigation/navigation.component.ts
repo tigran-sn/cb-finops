803d71ce58a1501fe4ee05e32b6c5a60cd3a9354
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-07 12:55:26 +0400

    Added logout logic

--- src/app/private/components/navigation/navigation.component.ts
+++ src/app/private/components/navigation/navigation.component.ts
@@ -5,6 +5,11 @@ import { IMenu } from '../../../core/infrastructure/interfaces';
 import { needConfirmation } from '../../../shared/decorators';
 
 import { PATHS } from './navigation.data';
+import { State, Store } from 'src/app/shared/store';
+import { Router } from '@angular/router';
+import { AuthService, HelperService } from 'src/app/core/services';
+import { Environment } from 'src/app/core/infrastructure/models';
+import { UserService } from '../../services/user';
 
 @Component({
   selector: 'app-navigation',
@@ -14,13 +19,24 @@ import { PATHS } from './navigation.data';
 export class NavigationComponent {
   paths: IMenu[] = PATHS;
 
-  constructor() {}
+  constructor(
+    private store: Store<State>,
+    private auth: AuthService,
+    private router: Router,
+    // private notificationService: NotificationService,
+    public helperService: HelperService,
+    private environment: Environment,
+    // private confirmationService: ConfirmationService,
+    private userService: UserService // public appMain: PrivateComponent,
+  ) // public app: AppComponent
+  {}
 
   @needConfirmation({
     title: 'Log Out',
     message: 'Are you sure you want to log out?',
   })
   logout(): void {
-    console.log('Logging out...');
+    this.helperService.setIgnoreGuardSetting(true);
+    this.auth.logOut({});
   }
 }
