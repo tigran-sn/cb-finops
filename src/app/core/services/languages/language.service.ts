09206170c2156e30c12a9a28551ee81f83e2def5
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-20 22:25:58 +0400

    Uncommented methods in languages service

--- src/app/core/services/languages/language.service.ts
+++ src/app/core/services/languages/language.service.ts
@@ -14,12 +14,12 @@ export class LanguageService {
   );
 
   constructor(
-    private translateService: TranslateService
-  ) // private localStorage: LocalStorageService
-  {
-    // if (this.localStorage.get('language')) {
-    //   this.setLanguage(this.localStorage.get('language').toString());
-    // }
+    private translateService: TranslateService,
+    private localStorage: LocalStorageService
+  ) {
+    if (this.localStorage.get('language')) {
+      this.setLanguage(this.localStorage.get('language').toString());
+    }
   }
 
   setLanguage(language: string): void {
@@ -39,16 +39,19 @@ export class LanguageService {
       }
     }
   }
+
   reset(): void {
     this.translateService.use(Languages.English);
     this.currentLanguage.next(Languages.English);
     moment.locale(MomentJSLocale.English);
-    // this.localStorage.remove('language');
+    this.localStorage.remove('language');
   }
-  // getCurrentLanguage(): string {
-  //   const localStorageLang = this.localStorage.get('language').toString();
-  //   return localStorageLang ? localStorageLang : Languages.English;
-  // }
+
+  getCurrentLanguage(): string {
+    const localStorageLang = this.localStorage.get('language').toString();
+    return localStorageLang ? localStorageLang : Languages.English;
+  }
+
   getTranslation(key: string): string {
     return this.translateService.instant(key);
   }
