bfc564375bb8413f434795b339c7f58a81fe37df
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-05-20 22:41:57 +0400

    Commented sorting related codes in filled reports page

--- src/app/private/components/reports/filled-reports/filled-reports.component.ts
+++ src/app/private/components/reports/filled-reports/filled-reports.component.ts
@@ -3,7 +3,7 @@ import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { MatSort, Sort } from '@angular/material/sort';
+// import { MatSort, Sort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 
 import { EditDialogComponent } from '../../edit-dialog/edit-dialog.component';
@@ -17,11 +17,11 @@ import { REPORTS } from '../../../mocks';
   styleUrls: ['./filled-reports.component.scss'],
 })
 export class FilledReportsComponent {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  // @ViewChild(MatSort) sort: MatSort;
+
   displayedColumns: string[] = ['reportName', 'dateTime'];
   dataSource = new MatTableDataSource<IReport>(REPORTS);
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
@@ -30,7 +30,7 @@ export class FilledReportsComponent {
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    // this.dataSource.sort = this.sort;
   }
 
   openEditDialog(item: any) {
@@ -48,18 +48,18 @@ export class FilledReportsComponent {
     });
   }
 
-  /** Announce the change in sort state for assistive technology. */
-  announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
+  // /** Announce the change in sort state for assistive technology. */
+  // announceSortChange(sortState: Sort) {
+  //   // This example uses English messages. If your application supports
+  //   // multiple language, you would internationalize these strings.
+  //   // Furthermore, you can customize the message to add additional
+  //   // details about the values being sorted.
+  //   if (sortState.direction) {
+  //     this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+  //   } else {
+  //     this._liveAnnouncer.announce('Sorting cleared');
+  //   }
+  // }
 
   edit(el: IReport): void {
     console.log('edit', el);
