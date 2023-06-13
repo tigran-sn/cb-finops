4e4fb828ff40b57cd74916920858f34b6867c5b1
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-06-13 08:10:07 +0400

    Translations small phase, Translated pagination to Armenian

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,7 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
@@ -55,10 +55,44 @@ const matModules = [
   NgxMatTimepickerModule,
 ];
 
+const armRangeLabel = (page: number, pageSize: number, length: number) => {
+  if (length == 0 || pageSize == 0) {
+    return `0 van ${length}`;
+  }
+
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize;
+
+  // If the start index exceeds the list length, do not try and fix the end index to the end.
+  const endIndex =
+    startIndex < length
+      ? Math.min(startIndex + pageSize, length)
+      : startIndex + pageSize;
+
+  return `${startIndex + 1} - ${endIndex} ${length}-ից`;
+};
+
+function getArmPaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+
+  paginatorIntl.itemsPerPageLabel = 'Նյութ մեկ էջում:';
+  paginatorIntl.nextPageLabel = 'Հաջորդ էջ';
+  paginatorIntl.lastPageLabel = 'Վերջին էջ';
+  paginatorIntl.previousPageLabel = 'Նախորդ էջ';
+  paginatorIntl.firstPageLabel = 'Առաջին էջ';
+  paginatorIntl.getRangeLabel = armRangeLabel;
+
+  return paginatorIntl;
+}
+
 @NgModule({
   imports: matModules,
   declarations: [],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MatPaginatorIntl, useValue: getArmPaginatorIntl() }
+  ],
   exports: matModules,
 })
 export class MaterialModule {}
