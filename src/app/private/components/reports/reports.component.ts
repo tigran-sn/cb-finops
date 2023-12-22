ef1679bc5d7ccbb6764a43ebade43f443bcbe427
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-12-22 10:37:49 +0400

    Refactor reports component to include filters and date range picker in server side paging

--- src/app/private/components/reports/reports.component.ts
+++ src/app/private/components/reports/reports.component.ts
@@ -1,27 +1,34 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-// import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { DatePipe } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import {
   MatTableDataSource,
   _MatTableDataSource,
 } from '@angular/material/table';
-// import { MatSort, Sort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 
-import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
+import { Observable, catchError, throwError } from 'rxjs';
+import { first, map } from 'rxjs/operators';
 
-import {IFilterData, IReport} from '../../interfaces';
-import { ReportsService } from '../../services';
 import { State, Store } from 'src/app/shared/store';
-import { ReportModel } from 'src/app/core/infrastructure/models';
+import {
+  FilterModel,
+  ReportModel,
+  ReportsLookupModel,
+} from 'src/app/core/infrastructure/models';
 import { IResponse } from 'src/app/core/infrastructure/interfaces';
-import { ListDataModel } from 'src/app/core/infrastructure/models/shared/list-data.model';
 import { ReportTypeEnum } from 'src/app/core/infrastructure/enums';
-import {appSettings} from "../../../app.settings";
-import {map, tap} from "rxjs/operators";
-import {catchError, of, startWith, switchMap} from "rxjs";
+import { CustomSnackbarService } from 'src/app/shared/services';
+
+import { IDealType, IReport, IReportResponse } from '../../interfaces';
+import { ReportsService } from '../../services';
+import { appSettings } from '../../../app.settings';
+import { LookupsService } from '../../services/lookups/lookups.service';
+import { EditDialogComponent } from '../edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'app-reports',
@@ -31,6 +38,14 @@ import {catchError, of, startWith, switchMap} from "rxjs";
 export class ReportsComponent implements OnInit, AfterViewInit {
   @ViewChild('paginator') paginator: MatPaginator;
 
+  controls: FilterModel;
+  form: FormGroup;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+  dealType = new FormControl();
+  dealTypes: IDealType[];
   totalData: number;
   dataSource = new MatTableDataSource<ReportModel>();
   pageSizes = [10, 20, 50, 100];
@@ -46,43 +61,28 @@ export class ReportsComponent implements OnInit, AfterViewInit {
     'calculationDate',
     'status',
   ];
+  pageSize = 10;
+  pageNumber = 1;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private matDialog: MatDialog,
     private reportsService: ReportsService,
-    private store: Store<State>
+    private store: Store<State>,
+    private fb: FormBuilder,
+    private lookupsService: LookupsService,
+    private customSnackbarService: CustomSnackbarService,
+    private datePipe: DatePipe
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getLookUps().pipe(first()).subscribe();
+    this.initForm();
+  }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.store.update({ showLoader: true });
-
-    this.paginator.page
-      .pipe(
-        tap(() => {
-          this.store.update({ showLoader: true });
-        }),
-        startWith({}),
-        switchMap(() => {
-          return this.getTableData$(
-            this.paginator.pageIndex + 1,
-            this.paginator.pageSize
-          ).pipe(catchError(() => of(null)));
-        }),
-        map((res) => {
-          if (res == null) return [];
-          this.totalData = res.data.totalCount;
-          return res.data.listItems;
-        })
-      )
-      .subscribe((res) => {
-        this.dataSource = new MatTableDataSource(res);
-        this.store.update({ showLoader: false });
-      });
+    this.fetchData(this.pageNumber, this.pageSize);
   }
 
   openEditDialog(item: any) {
@@ -117,25 +117,73 @@ export class ReportsComponent implements OnInit, AfterViewInit {
     });
   }
 
-  filter(filterData: IFilterData): void {
-    // this.store.update({ showLoader: true });
-    // this.reportsService
-    //   .getReports(ReportTypeEnum.All, filterData)
-    //   .subscribe((res: IResponse<ListDataModel<ReportModel>>) => {
-    //     if (res.success) {
-    //       this.dataSource = new _MatTableDataSource(res.data.listItems);
-    //       this.dataSource.paginator = this.paginator;
-    //       // this.dataSource.sort = this.sort;
-    //
-    //       this.store.update({ showLoader: false });
-    //     }
-    //   });
+  onPageEvent(event: PageEvent) {
+    this.fetchData(event.pageIndex + 1, event.pageSize);
   }
 
-  private getTableData$(pageNumber: number, pageSize: number) {
-    return this.reportsService.getReports(ReportTypeEnum.All, {}, {
-      pageSize,
-      pageNumber,
+  applyFilter() {
+    this.fetchData(this.paginator.pageIndex + 1, this.paginator.pageSize);
+  }
+
+  resetFilter(): void {
+    this.form.reset();
+    this.fetchData(this.paginator.pageIndex + 1, this.paginator.pageSize);
+  }
+
+  private initForm() {
+    this.form = this.fb.group({
+      dealType: this.dealType,
+      range: this.range,
     });
+    this.setControls();
+  }
+
+  private setControls() {
+    this.controls = {
+      dealType: this.form.get('dealType') as FormControl,
+      range: this.form.get('range') as FormGroup,
+    };
+  }
+
+  private getLookUps(): Observable<IResponse<ReportsLookupModel>> {
+    return this.lookupsService.getLookUps().pipe(
+      map((res: IResponse<ReportsLookupModel>) => {
+        if (res.success) {
+          this.dealTypes = res.data.dealTypes;
+        }
+        return res;
+      })
+    );
+  }
+
+  private fetchData(pageNumber: number, pageSize: number) {
+    this.store.update({ showLoader: true });
+
+    const dealType = this.form.get('dealType')?.value || null;
+    const start =
+      this.datePipe.transform(this.range?.get('start')?.value, 'dd-MM-yyyy') ||
+      null;
+    const end =
+      this.datePipe.transform(this.range?.get('end')?.value, 'dd-MM-yyyy') ||
+      null;
+
+    const queryString = `&pageNumber=${pageNumber}&pageSize=${pageSize}${
+      dealType ? `&dealType=${dealType}` : ''
+    }${start ? `&startDate=${start}` : ''}${end ? `&endDate=${end}` : ''}`;
+
+    this.reportsService
+      .getReportsNew(ReportTypeEnum.All, queryString)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          this.customSnackbarService.openSnackbar(err.message, 'error');
+          this.store.update({ showLoader: false });
+          return throwError(err);
+        })
+      )
+      .subscribe((res: IReportResponse) => {
+        this.totalData = res.pagination.totalCount;
+        this.dataSource = new MatTableDataSource(res.reports);
+        this.store.update({ showLoader: false });
+      });
   }
 }
--- src/app/private/services/reports/reports.service.ts
+++ src/app/private/services/reports/reports.service.ts
@@ -3,7 +3,7 @@ import { HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { HttpService } from 'src/app/http';
 
-import {IPartner, IResponse} from 'src/app/core/infrastructure/interfaces';
+import { IPartner, IResponse } from 'src/app/core/infrastructure/interfaces';
 import { ReportModel } from 'src/app/core/infrastructure/models/reports/report.model';
 import { ListDataModel } from 'src/app/core/infrastructure/models/shared/list-data.model';
 import { REPORTS_API_URL } from './reports.url';
@@ -11,14 +11,19 @@ import {
   PartnerModel,
   ReportCreationModel,
   ReportsLookupModel,
-  ReportsSearchModel
+  ReportsSearchModel,
 } from 'src/app/core/infrastructure/models';
 
-import {ISOCODES, REPORTS} from '../../mocks';
-import { ReportTypeEnum } from "../../../core/infrastructure/enums";
-import {DEAL_TYPES} from "../../mocks/deal-types.mock";
-import {map} from "rxjs/operators";
-import {IFilterData, IPaginationParams, IReport, IReportResponse} from "../../interfaces";
+import { ISOCODES, REPORTS } from '../../mocks';
+import { ReportTypeEnum } from '../../../core/infrastructure/enums';
+import { DEAL_TYPES } from '../../mocks/deal-types.mock';
+import { map } from 'rxjs/operators';
+import {
+  IFilterData,
+  IPaginationParams,
+  IReport,
+  IReportResponse,
+} from '../../interfaces';
 
 @Injectable()
 export class ReportsService {
@@ -31,21 +36,27 @@ export class ReportsService {
       params = params.appendAll({
         ...(filterData.dealType && { dealType: filterData.dealType }),
         ...(filterData.range && {
-          ...filterData.range.start && { 'startDate': filterData.range.start },
-          ...filterData.range.end && { 'endDate': filterData.range.end }
-        })
+          ...(filterData.range.start && { startDate: filterData.range.start }),
+          ...(filterData.range.end && { endDate: filterData.range.end }),
+        }),
       });
     }
     return '&' + params.toString();
   }
 
-  private generatePaginationParams(paginationParams: IPaginationParams): string {
+  private generatePaginationParams(
+    paginationParams: IPaginationParams
+  ): string {
     let params = new HttpParams();
 
     if (paginationParams) {
       params = params.appendAll({
-        ...(paginationParams.pageSize && { pageSize: paginationParams.pageSize }),
-        ...(paginationParams.pageNumber && { pageNumber: paginationParams.pageNumber }),
+        ...(paginationParams.pageSize && {
+          pageSize: paginationParams.pageSize,
+        }),
+        ...(paginationParams.pageNumber && {
+          pageNumber: paginationParams.pageNumber,
+        }),
       });
     }
     return '&' + params.toString();
@@ -54,28 +65,47 @@ export class ReportsService {
   getReports(
     reportType: number,
     filterData?: IFilterData,
-    paginationParams?: IPaginationParams,
+    paginationParams?: IPaginationParams
   ): Observable<IResponse<ListDataModel<ReportModel>>> {
-    let queryParams = filterData?.dealType || filterData?.range?.start || filterData?.range?.end
-      ? this.generateQueryParams(filterData)
-      : '';
-    queryParams += paginationParams?.pageNumber ?? paginationParams?.pageSize
-      ? this.generatePaginationParams(paginationParams)
-      : '';
-    return this.httpService.get<IReportResponse>(`${REPORTS_API_URL.getReports}?status=${reportType}${queryParams}`)
-      .pipe(map((res: IReportResponse) => {
-        return {
-          success: true,
-          data: {
-            totalCount: res.pagination.totalCount,
-            listItems: [...res.reports],
-          },
-        };
-      }));
+    let queryParams =
+      filterData?.dealType || filterData?.range?.start || filterData?.range?.end
+        ? this.generateQueryParams(filterData)
+        : '';
+    queryParams +=
+      paginationParams?.pageNumber ?? paginationParams?.pageSize
+        ? this.generatePaginationParams(paginationParams)
+        : '';
+    return this.httpService
+      .get<IReportResponse>(
+        `${REPORTS_API_URL.getReports}?status=${reportType}${queryParams}`
+      )
+      .pipe(
+        map((res: IReportResponse) => {
+          return {
+            success: true,
+            data: {
+              totalCount: res.pagination.totalCount,
+              listItems: [...res.reports],
+            },
+          };
+        })
+      );
+  }
+
+  getReportsNew(
+    status: number,
+    queryParams: string
+  ): Observable<IReportResponse> {
+    return this.httpService.get<IReportResponse>(
+      `${REPORTS_API_URL.getReports}?status=${status}${queryParams}`
+    );
   }
 
   sendReports(reportIdList: number[]): Observable<string> {
-    return this.httpService.post(`${REPORTS_API_URL.sendReports}`, reportIdList);
+    return this.httpService.post(
+      `${REPORTS_API_URL.sendReports}`,
+      reportIdList
+    );
   }
 
   createReport(reportBody: ReportCreationModel): Observable<string> {
@@ -90,20 +120,23 @@ export class ReportsService {
         statuses: [],
       },
       success: true,
-    })
+    });
     return this.httpService.get(`${REPORTS_API_URL.getLookUps}`);
   }
 
   getPartnersList(): Observable<IResponse<ListDataModel<PartnerModel>>> {
-    return this.httpService.get<PartnerModel[]>(`${REPORTS_API_URL.getPartnersList}`)
-      .pipe(map((res: PartnerModel[]) => {
-        return {
-          success: true,
-          data: {
-            totalCount: res.length,
-            listItems: [...res],
-          },
-        };
-      }));
+    return this.httpService
+      .get<PartnerModel[]>(`${REPORTS_API_URL.getPartnersList}`)
+      .pipe(
+        map((res: PartnerModel[]) => {
+          return {
+            success: true,
+            data: {
+              totalCount: res.length,
+              listItems: [...res],
+            },
+          };
+        })
+      );
   }
 }
