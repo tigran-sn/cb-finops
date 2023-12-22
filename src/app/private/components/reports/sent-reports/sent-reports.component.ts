52c809a29c946523ae6208e08d7fd35716baa128
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2023-12-23 00:02:23 +0400

    Refactor SentReportsComponent

--- src/app/private/components/reports/sent-reports/sent-reports.component.ts
+++ src/app/private/components/reports/sent-reports/sent-reports.component.ts
@@ -1,28 +1,32 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatPaginator, PageEvent} from "@angular/material/paginator";
-import {_MatTableDataSource, MatTableDataSource} from "@angular/material/table";
-import {ReportModel} from "../../../../core/infrastructure/models";
-import {ActivatedRoute, Router} from "@angular/router";
-import {MatDialog} from "@angular/material/dialog";
-import {ReportsService} from "../../../services";
-import {State, Store} from "../../../../shared/store";
-import {ReportTypeEnum} from "../../../../core/infrastructure/enums";
-import {IResponse} from "../../../../core/infrastructure/interfaces";
-import {ListDataModel} from "../../../../core/infrastructure/models/shared/list-data.model";
-import {EditDialogComponent} from "../../edit-dialog/edit-dialog.component";
-import {IFilterData, IReport} from "../../../interfaces";
-import {appSettings} from "../../../../app.settings";
-import {catchError, of, startWith, switchMap} from "rxjs";
-import {map, tap} from "rxjs/operators";
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog';
+
+import { catchError, throwError } from 'rxjs';
+
+import { State, Store } from 'src/app/shared/store';
+import { ReportModel } from 'src/app/core/infrastructure/models';
+import { ReportTypeEnum } from 'src/app/core/infrastructure/enums';
+import { CustomSnackbarService } from 'src/app/shared/services';
+
+import { IFilterData, IReport, IReportResponse } from '../../../interfaces';
+import { ReportsService } from '../../../services';
+import { appSettings } from '../../../../app.settings';
+import { EditDialogComponent } from '../../edit-dialog/edit-dialog.component';
 
 @Component({
   selector: 'app-sent-reports',
   templateUrl: './sent-reports.component.html',
   styleUrls: ['./sent-reports.component.scss'],
 })
-export class SentReportsComponent {
+export class SentReportsComponent implements AfterViewInit {
   @ViewChild('paginator') paginator: MatPaginator;
 
+  filterData: IFilterData;
   totalData: number;
   dataSource = new MatTableDataSource<ReportModel>();
   pageSizes = [10, 20, 50, 100];
@@ -38,43 +42,30 @@ export class SentReportsComponent {
     'calculationDate',
     'status',
   ];
+  pageSize = 10;
+  pageNumber = 1;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private matDialog: MatDialog,
     private reportsService: ReportsService,
-    private store: Store<State>
+    private store: Store<State>,
+    private customSnackbarService: CustomSnackbarService
   ) {}
 
-  ngOnInit(): void {}
-
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.store.update({ showLoader: true });
+    this.fetchData(this.pageNumber, this.pageSize);
+  }
 
-    this.paginator.page
-      .pipe(
-        tap(() => {
-          this.store.update({ showLoader: true });
-        }),
-        startWith({}),
-        switchMap(() => {
-          return this.getTableData$(
-            this.paginator.pageIndex + 1,
-            this.paginator.pageSize
-          ).pipe(catchError(() => of(null)));
-        }),
-        map((res) => {
-          if (res == null) return [];
-          this.totalData = res.data.totalCount;
-          return res.data.listItems;
-        })
-      )
-      .subscribe((res) => {
-        this.dataSource = new MatTableDataSource(res);
-        this.store.update({ showLoader: false });
-      });
+  onFilterApplied(filterData: IFilterData): void {
+    this.filterData = filterData;
+    this.fetchData(this.paginator.pageIndex + 1, this.paginator.pageSize);
+  }
+
+  onFilterReset(): void {
+    this.filterData = {};
+    this.fetchData(this.paginator.pageIndex + 1, this.paginator.pageSize);
   }
 
   openEditDialog(item: any) {
@@ -109,25 +100,34 @@ export class SentReportsComponent {
     });
   }
 
-  filter(filterData: IFilterData): void {
-    // this.store.update({ showLoader: true });
-    // this.reportsService
-    //   .getReports(ReportTypeEnum.Sent, filterData)
-    //   .subscribe((res: IResponse<ListDataModel<ReportModel>>) => {
-    //     if (res.success) {
-    //       this.dataSource = new _MatTableDataSource(res.data.listItems);
-    //       this.dataSource.paginator = this.paginator;
-    //       // this.dataSource.sort = this.sort;
-    //
-    //       this.store.update({ showLoader: false });
-    //     }
-    //   });
+  onPageEvent(event: PageEvent) {
+    this.fetchData(event.pageIndex + 1, event.pageSize);
   }
 
-  private getTableData$(pageNumber: number, pageSize: number) {
-    return this.reportsService.getReports(ReportTypeEnum.Sent, {}, {
-      pageSize,
-      pageNumber,
-    });
+  private fetchData(pageNumber: number, pageSize: number) {
+    this.store.update({ showLoader: true });
+
+    const dealType = this.filterData?.dealType || null;
+    const start = this.filterData?.range?.start || null;
+    const end = this.filterData?.range?.end || null;
+
+    const queryString = `&pageNumber=${pageNumber}&pageSize=${pageSize}${
+      dealType ? `&dealType=${dealType}` : ''
+    }${start ? `&startDate=${start}` : ''}${end ? `&endDate=${end}` : ''}`;
+
+    this.reportsService
+      .getReportsNew(ReportTypeEnum.Sent, queryString)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          this.customSnackbarService.openSnackbar(err.message, 'error');
+          this.store.update({ showLoader: false });
+          return throwError(err);
+        })
+      )
+      .subscribe((res: IReportResponse) => {
+        this.totalData = res.pagination.totalCount;
+        this.dataSource = new MatTableDataSource(res.reports);
+        this.store.update({ showLoader: false });
+      });
   }
 }
